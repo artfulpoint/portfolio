0539e27fe55c3588f05518c61a158426a7ff3992
artfulpoint <79894555+artfulpoint@users.noreply.github.com>
2025-08-26 18:33:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function clicked(e) {
+  let theme_name = e.getAttribute("src");
+  theme_name = theme_name.replace("images/", "");
+  theme_name = theme_name.replace(".png", "");
+
+  if (theme_name == "light") {
+    e.setAttribute("src", "images/dark.png");
+    let body_theme = document.getElementsByClassName("light");
+
+    body_theme[0].setAttribute("class", "dark");
+  } else {
+    e.setAttribute("src", "images/light.png");
+    let body_theme = document.getElementsByClassName("dark");
+    body_theme[0].setAttribute("class", "light");
+  }
+}
+
+// function scrolling(e) {
+//   let header = document.getElementById("header");
+//   let href = e.textContent;
+//   //   e.getAttribute("href");
+//   //   href = href.replace("#", "");
+//   console.log(href);
+//   let element = document.getElementById(href);
+//   console.log(header);
+//   console.log(element);
+//   let headerOffSetHeight = header.offsetHeight;
+//   let elementOffSetHeight = element.offsetHeight;
+//   console.log(headerOffSetHeight);
+//   console.log(elementOffSetHeight);
+//   element.addEventListener("click", function (event) {
+//     event.preventDefault();
+//   });
+//   elementOffSetHeight - headerOffSetHeight;
+//   window.scrollTo({ top: elementOffSetHeight, behavior: "smooth" });
+//   //   let links = document.getElementById("inner-body").addEventListener;
+//   //   links.scrollTo({ top: 125, behavior: "smooth" });
+// }
